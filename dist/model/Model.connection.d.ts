ac9abe3d2204b021d52644d8e0bd8a85ebe5cbcd
YYP <youngyongpark@gmail.com>
2021-03-18 12:19:04 +0900

    Update dependencies, Bump Version 0.1.0 (#63)
    * Deps
    
    * deps
    
    * fix types

--- dist/model/Model.connection.d.ts
+++ dist/model/Model.connection.d.ts
@@ -1,18 +1,18 @@
 /// <reference types="node" />
-import knex from "knex";
+import { Knex } from "knex";
 import { ModelConnectivitySettings } from "./Model";
 import { Repository } from "./Model.entity.repository";
 import { ClassType } from "../types";
 export declare type QueryParameterTypes = string | number | bigint | boolean | null | Date | Array<string> | Array<number> | Array<Date> | Array<boolean> | Buffer;
 export declare class TransactionScope {
-    readonly kx: knex;
-    constructor(kx: knex);
+    readonly kx: Knex;
+    constructor(kx: Knex);
     execute(query: string, params?: Array<QueryParameterTypes>): Promise<any>;
     getRepository<T>(repository: ClassType<T>): Repository<T>;
 }
 export declare class ConnectionManager {
     private readonly kx;
-    constructor(kx: knex);
+    constructor(kx: Knex);
     static getConnectionManager(settings: ModelConnectivitySettings): ConnectionManager;
     transaction<T>(ctx: (conn: TransactionScope) => Promise<T>): Promise<T>;
 }

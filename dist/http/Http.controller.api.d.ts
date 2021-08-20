13d5a2aa3393fb3f612e7a626503d36739370f79
YYP <youngyongpark@gmail.com>
2021-08-20 11:34:37 +0900

    Change absolute path to relative path (#81)

--- dist/http/Http.controller.api.d.ts
+++ dist/http/Http.controller.api.d.ts
@@ -1,9 +1,9 @@
-import { Cyan } from "src/core";
-import { CyanRequest } from "src/types/Handler";
 import { Controller } from "./Http.controller";
 import { HttpError } from "./Http.error";
 import { HttpRequest as HttpRequest } from "./Http.request";
 import { HttpResponse } from "./Http.response";
+import { Cyan } from "../core";
+import { CyanRequest } from "../types/Handler";
 export declare class ApiController extends Controller {
     afterHandle(request: HttpRequest, response: any): Promise<HttpResponse>;
     onHttpError(request: HttpRequest, error: HttpError): Promise<HttpError>;

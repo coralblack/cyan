9d4f6e50dedbeabf2aec77dd4d81488e2ebf2dfe
Dongjun Shin <tlsehdwns239@gmail.com>
2022-04-18 15:52:05 +0900

    - path.resolve 제거 및 windows path를 고려한 코드 적용

--- src/core/Application.ts
+++ src/core/Application.ts
@@ -145,7 +145,8 @@ export class Cyan {
   }
 
   private initHandler(controller: HttpController, route: RouteMetadataArgs) {
-    const path = this.settings.os ? route.path : resolve(this.settings.basePath || "/", route.path);
+    const basePath = this.settings.basePath?.endsWith("/") ? this.settings.basePath.slice(0, -1) : this.settings.basePath || "";
+    const path = (basePath => (route.path.startsWith("/") ? `${basePath}${route.path}` : `${basePath}/${route.path}`))(basePath);
 
     this.logger.info(`[router] ${route.action} ${path} - ${route.target.name}.${route.method}`);
 
